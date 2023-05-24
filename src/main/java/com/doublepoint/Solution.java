52e2a6c6d55a1ada5f191f0a687acd0926004450
gogogo1024 <jxycbjhc@163.com>
2023-05-24 11:40:55 +0800

    feat: add pickAppleTree

--- src/main/java/com/doublepoint/Solution.java
+++ src/main/java/com/doublepoint/Solution.java
@@ -40,4 +40,108 @@ public class Solution {
 
     }
 
+    /*
+    买卖股票的最佳时机
+    stockPrice[i]是第i天的股票价格，你可以选择买入或者卖出
+    你最多可以有两次交易，也就是买入卖出，买入卖出（不允许买入买入卖出卖出）
+    输入：stockPrice = [4,4,6,1,1,4,2,5]
+    输出： 6
+     */
+
+    /**
+     * 买入卖出两次的最大利润
+     * 利用隔板法，隔离出两次的买入卖出
+     *
+     * @param stockPrice 股票价格
+     * @return 最大利润
+     */
+    public int maxProfit(int[] stockPrice) {
+        int len = stockPrice.length;
+        int maxCost = 0;
+        for (int i = 0; i < len; i++) {
+            // 枚举隔板 [0,i),[i,len)
+            int leftCost = getCost(stockPrice, 0, i);
+            int rightCost = getCost(stockPrice, i, len);
+            maxCost = Math.max(maxCost, leftCost + rightCost);
+        }
+        return maxCost;
+    }
+
+    /**
+     * 买入卖出一次的最大利润
+     *
+     * @param stockPrice 股票价格
+     * @param left       左边界
+     * @param right      有边界
+     * @return 最大利润
+     */
+    private int getCost(int[] stockPrice, int left, int right) {
+        int minPrice = Integer.MAX_VALUE;
+        int cost = 0;
+        for (int i = left; i < right; i++) {
+            // 维护到i为止的最小stockPrice
+            minPrice = Math.min(minPrice, stockPrice[i]);
+            cost = Math.max(cost, stockPrice[i] - minPrice);
+        }
+        return cost;
+    }
+
+    /*
+    捡苹果
+    A和B在连续标记为1~N的苹果树上分别收集连续K和L颗树上的苹果
+    A和B选择的区间不重合,返回他们能收集到的最多苹果
+    输入：appleTree =[6,1,4,6,3,2,7,4], K =3,L=2
+    输出：24 ([4,6,4],[7,4])
+     */
+    public int pickAppleTree(int[] appleTree, int K, int L) {
+        int len = appleTree.length;
+        int maxCount = Integer.MIN_VALUE;
+        for (int i = 0; i < len; i++) {
+            int leftMaxL = findMax(appleTree, L, 0, i);
+            int rightMaxK = findMax(appleTree, K, i, len);
+            int leftMaxK = findMax(appleTree, K, 0, i);
+            int rightMaxL = findMax(appleTree, L, i, len);
+            if (leftMaxK != -1 && rightMaxL != -1) {
+                maxCount = Math.max(maxCount, leftMaxK + rightMaxL);
+            }
+            if (leftMaxL != -1 && rightMaxK != -1) {
+                maxCount = Math.max(maxCount, leftMaxL + rightMaxK);
+            }
+        }
+        if (maxCount == Integer.MIN_VALUE) {
+            return -1;
+        }
+        return maxCount;
+    }
+
+    /**
+     * 采集连续k颗苹果树的最大苹果个数
+     *
+     * @param appleTree 苹果树上的苹果
+     * @param k         连续的k颗苹果树
+     * @param start     开始位置
+     * @param end       结束位置
+     * @return 能采集的最大苹果个数
+     */
+    private int findMax(int[] appleTree, int k, int start, int end) {
+        if (k > end - start) {
+            return -1;
+        }
+        int apples = 0, maxApples = 0;
+        for (int i = start; i < start + k; i++) {
+            apples += appleTree[i];
+
+        }
+        maxApples = apples;
+        int left = start, right = start + k;
+        while (right < end) {
+            apples -= appleTree[left];
+            apples += appleTree[right];
+            maxApples = Math.max(maxApples, apples);
+            left++;
+            right++;
+        }
+        return maxApples;
+    }
+
 }
--- src/test/java/com/doublepoint/SolutionTest.java
+++ src/test/java/com/doublepoint/SolutionTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
     @Test
-    void testForkDistinctCharacters() {
+    void testForFindMaxWeights() {
         Solution solution = new Solution();
         int[] customers = new int[]{1, 0, 1, 2, 1, 1, 7, 5};
         int[] grumpy = new int[]{0, 1, 0, 1, 0, 1, 0, 1};
@@ -14,4 +14,22 @@ public class SolutionTest {
         int radius1 = solution.findMaxWeights(customers, grumpy, k);
         assertEquals(16, radius1);
     }
+    @Test
+    void testForMaxProfit() {
+        Solution solution = new Solution();
+        int[] stockPrice = new int[]{ 4,4,6,1,1,4,2,5};
+        int maxCost = solution.maxProfit(stockPrice);
+        assertEquals(6, maxCost);
+    }
+    @Test
+    void testForPickAppleTree() {
+        Solution solution = new Solution();
+        int[] appleTree = new int[]{ 6,1,4,6,3,2,7,4};
+        int K = 3, L =2;
+
+        int maxAppleCount = solution.pickAppleTree(appleTree,K,L);
+        assertEquals(24, maxAppleCount);
+    }
+
 }
+
